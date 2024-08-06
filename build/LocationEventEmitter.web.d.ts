cc814cb3aa04195e2a8b373c30b4fb5db6d01304
UpinBen <ben@upin.co>
2024-08-06 16:47:44 -0400

    initial commit from expo-location

---
+++ build/LocationEventEmitter.web.d.ts
@@ -0,0 +1,2 @@
+export declare const LocationEventEmitter: import("expo-modules-core/build/ts-declarations/EventEmitter").EventEmitter<Record<never, never>>;
+//# sourceMappingURL=LocationEventEmitter.web.d.ts.map
